e37547de9d3925b21674593bd1a9a627697379f0
Helmut Drewes <helmut@contagra.com>
2023-05-17 08:54:13 +0200

    Add purchase stock catch weight module

---
+++ purchase_stock_catch_weight/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Purchase Catch Weight',
+    'version': '1.0.0',
+    'category': 'Warehouse',
+    'depends': [
+        'purchase_stock',
+    ],
+    'description': """
+    """,
+    'author': 'Hibou Corp., Contagra',
+    'license': 'AGPL-3',
+    'website': 'https://hibou.io/',
+    'data': [
+    ],
+    'installable': True,
+    'application': False,
+}
